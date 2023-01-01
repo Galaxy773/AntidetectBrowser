1bbf7de676756070759084b6c66fe4f89e499b5d
Alexander Belkin <galaxycode.mc@yandex.ru>
2023-01-01 04:37:31 +0300

    Create fingerprint.heap.js

---
+++ evasions/fingerprint.heap.js
@@ -0,0 +1,38 @@
+(browserType, fakeHeap) => {
+    if (Performance !== 'undefined') {
+        //todo: проверить
+        let _Object = utils.cache.Object;
+        const _Reflect = utils.cache.Reflect;
+        let proto = _Object.getPrototypeOf(Performance);
+
+        if (browserType === 'FIREFOX' || browserType === 'SAFARI') {
+            delete proto.memory;
+            delete proto.measureUserAgentSpecificMemory;
+            if (browserType === 'SAFARI') {
+                delete proto.eventCounts;
+            }
+            return;
+        }
+
+        if (browserType === 'OPERA') {
+            delete proto.getEntriesByName;
+            delete proto.getEntries;
+            delete proto.toJSON;
+        } else {
+            //todo: toJSON
+        }
+
+        utils.replaceGetterWithProxy(proto, 'memory', {
+            apply(target, thisArg, args) {
+                let memoryObj = _Reflect.apply(target, thisArg, args);
+                let newMemoryObj = {
+                    jsHeapSizeLimit: fakeHeap,
+                    totalJSHeapSize: memoryObj.totalJSHeapSize,
+                    usedJSHeapSize: memoryObj.usedJSHeapSize
+                }
+                Object.setPrototypeOf(newMemoryObj, memoryObj);
+                return newMemoryObj;
+            }
+        });
+    }
+}
